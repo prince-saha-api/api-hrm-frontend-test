d12d13efd7a0404c8b16684eb3f3911b58a56778
nazmulapi <nazmulhussain.api@gmail.com>
2024-08-22 16:09:08 +0600

    Fix profile edit

--- src/components/app-modules/ProfileView/ProfileEdit.jsx
+++ src/components/app-modules/ProfileView/ProfileEdit.jsx
@@ -1,24 +1,21 @@
 import React, { useState } from "react";
 import useSWR from "swr";
 import { useForm } from "@mantine/form";
-import {
-  Modal,
-  Button,
-  Group,
-  Grid,
-  TextInput,
-  Select,
-  NumberInput,
-} from "@mantine/core";
+import { Modal, Button, Group, Grid, TextInput, Select } from "@mantine/core";
 import { DateInput } from "@mantine/dates";
 import { toast } from "react-toastify";
+import { useUser } from "@/components/contexts/UserContext";
 import { update } from "@/lib/submit";
 import { fetcher } from "@/lib/fetch";
-import { getFullName } from "@/lib/helper";
+import { formatDateToYYYYMMDD, getFullName } from "@/lib/helper";
 import { validateEmail, validatePhoneNumber } from "@/lib/validate";
+import { bloodGroups, genders, maritalStatus } from "@/data";
 
 const Index = ({ opened, close, item, setItem }) => {
   const [isSubmitting, setIsSubmitting] = useState(false);
+
+  const { user, setUser } = useUser();
+  const userId = user?.id;
 
   const [isMarried, setIsMarried] = useState(
     item?.marital_status === "Married"
@@ -29,10 +26,10 @@ const Index = ({ opened, close, item, setItem }) => {
     initialValues: {
       first_name: item?.first_name || "",
       last_name: item?.last_name || "",
-      designation: item?.designation?.id.toString() || "",
-      official_id: item?.official_id || "",
+      // designation: item?.designation?.id.toString() || "",
+      // official_id: item?.official_id || "",
       // department: item?.departmenttwo?.[0]?.id || "",
-      joining_date: item?.joining_date ? new Date(item?.joining_date) : null,
+      // joining_date: item?.joining_date ? new Date(item?.joining_date) : null,
       personal_phone: item?.personal_phone || "",
       personal_email: item?.personal_email || "",
       dob: item?.dob ? new Date(item?.dob) : null,
@@ -46,10 +43,10 @@ const Index = ({ opened, close, item, setItem }) => {
       first_name: (value) =>
         value.length < 2 ? "First Name must be at least 2 characters" : null,
       last_name: (value) => (!value ? "Last Name is required" : null),
-      designation: (value) => (!value ? "Designation is required" : null),
-      official_id: (value) => (!value ? "Employee ID is required" : null),
+      // designation: (value) => (!value ? "Designation is required" : null),
+      // official_id: (value) => (!value ? "Employee ID is required" : null),
       // department: (value) => (!value ? "Department is required" : null),
-      joining_date: (value) => (!value ? "Joining Date is required" : null),
+      // joining_date: (value) => (!value ? "Joining Date is required" : null),
       personal_phone: (value) =>
         !value
           ? "Phone is required"
@@ -78,19 +75,19 @@ const Index = ({ opened, close, item, setItem }) => {
     setIsMarried(value === "Married");
   });
 
-  const {
-    data: designationsData,
-    error: designationsError,
-    isLoading: isDesignationsLoading,
-  } = useSWR(`/api/user/get-dsignation/`, fetcher, {
-    errorRetryCount: 2,
-    keepPreviousData: true,
-  });
+  // const {
+  //   data: designationsData,
+  //   error: designationsError,
+  //   isLoading: isDesignationsLoading,
+  // } = useSWR(`/api/user/get-dsignation/`, fetcher, {
+  //   errorRetryCount: 2,
+  //   keepPreviousData: true,
+  // });
 
-  const designations = designationsData?.data?.result?.map((item) => ({
-    label: item?.name?.toString() || "",
-    value: item?.id.toString() || "",
-  }));
+  // const designations = designationsData?.data?.result?.map((item) => ({
+  //   label: item?.name?.toString() || "",
+  //   value: item?.id.toString() || "",
+  // }));
 
   const {
     data: employeesData,
@@ -107,18 +104,16 @@ const Index = ({ opened, close, item, setItem }) => {
   }));
 
   const handleSubmit = async (values) => {
-    const formattedDOB = values.dob
-      ? values.dob.toISOString().split("T")[0]
-      : null;
+    const formattedDOB = formatDateToYYYYMMDD(values.dob);
 
-    const formattedJoiningDate = values.joining_date
-      ? values.joining_date.toISOString().split("T")[0]
-      : null;
+    // const formattedJoiningDate = values.joining_date
+    //   ? values.joining_date.toISOString().split("T")[0]
+    //   : null;
 
     const updatedValues = {
       ...values,
       dob: formattedDOB,
-      joining_date: formattedJoiningDate,
+      // joining_date: formattedJoiningDate,
     };
 
     setIsSubmitting(true);
@@ -137,10 +132,10 @@ const Index = ({ opened, close, item, setItem }) => {
           ...prev,
           first_name: response?.data?.first_name,
           last_name: response?.data?.last_name,
-          designation: response?.data?.designation,
-          official_id: response?.data?.official_id,
+          // designation: response?.data?.designation,
+          // official_id: response?.data?.official_id,
           // department: response?.data?.departmenttwo?.[0]?.id,
-          joining_date: response?.data?.joining_date,
+          // joining_date: response?.data?.joining_date,
           personal_phone: response?.data?.personal_phone,
           personal_email: response?.data?.personal_email,
           dob: response?.data?.dob,
@@ -150,6 +145,15 @@ const Index = ({ opened, close, item, setItem }) => {
           spouse_name: response?.data?.spouse_name,
           supervisor: response?.data?.supervisor,
         }));
+
+        if (item?.id == userId) {
+          setUser((prev) => ({
+            ...prev,
+            first_name: response?.data?.first_name,
+            last_name: response?.data?.last_name,
+          }));
+        }
+
         toast.success("Profile updated successfully");
       } else {
         toast.error(
@@ -193,6 +197,16 @@ const Index = ({ opened, close, item, setItem }) => {
           )}
         >
           <Grid>
+            {/* <Grid.Col span={12}>
+              <TextInput
+                mb="sm"
+                label="Employee ID"
+                placeholder="Employee ID"
+                required={true}
+                disabled={true}
+                {...form.getInputProps("official_id")}
+              />
+            </Grid.Col> */}
             <Grid.Col span={6}>
               <TextInput
                 mb="sm"
@@ -203,7 +217,7 @@ const Index = ({ opened, close, item, setItem }) => {
                 {...form.getInputProps("first_name")}
               />
 
-              <Select
+              {/* <Select
                 mb="sm"
                 label="Designation"
                 placeholder="Designation"
@@ -211,32 +225,22 @@ const Index = ({ opened, close, item, setItem }) => {
                 disabled={isSubmitting}
                 data={designations}
                 {...form.getInputProps("designation")}
-              />
+              /> */}
 
-              <TextInput
-                mb="sm"
-                label="Employee ID"
-                placeholder="Employee ID"
-                required={true}
-                disabled={true}
-                {...form.getInputProps("official_id")}
-              />
-
-              <DateInput
+              {/* <DateInput
                 mb="sm"
                 label="Date of Join"
                 placeholder="Pick date"
                 required={true}
                 disabled={isSubmitting}
                 {...form.getInputProps("joining_date")}
-              />
+              /> */}
               <TextInput
                 mb="sm"
                 label="Phone"
                 placeholder="Phone"
                 required={true}
                 disabled={isSubmitting}
-                // hideControls
                 {...form.getInputProps("personal_phone")}
               />
               <Select
@@ -248,6 +252,25 @@ const Index = ({ opened, close, item, setItem }) => {
                 data={employees}
                 {...form.getInputProps("supervisor")}
                 key={form.key("supervisor")}
+              />
+              <Select
+                mb="sm"
+                label="Blood Group"
+                placeholder="Blood Group"
+                required={true}
+                disabled={isSubmitting}
+                data={bloodGroups}
+                {...form.getInputProps("blood_group")}
+              />
+
+              <Select
+                mb="sm"
+                label="Marital Status"
+                placeholder="Marital Status"
+                required={true}
+                disabled={isSubmitting}
+                data={maritalStatus}
+                {...form.getInputProps("marital_status")}
               />
             </Grid.Col>
             <Grid.Col span={6}>
@@ -270,7 +293,7 @@ const Index = ({ opened, close, item, setItem }) => {
               <DateInput
                 mb="sm"
                 label="Date of Birth"
-                placeholder="Pick date"
+                placeholder="Date of Birth"
                 required={true}
                 disabled={isSubmitting}
                 {...form.getInputProps("dob")}
@@ -281,36 +304,8 @@ const Index = ({ opened, close, item, setItem }) => {
                 placeholder="Gender"
                 required={true}
                 disabled={isSubmitting}
-                data={["Male", "Female", "Other"]}
+                data={genders}
                 {...form.getInputProps("gender")}
-              />
-              <Select
-                mb="sm"
-                label="Blood Group"
-                placeholder="Blood Group"
-                required={true}
-                disabled={isSubmitting}
-                data={[
-                  "A+",
-                  "A-",
-                  "B+",
-                  "B-",
-                  "AB+",
-                  "AB-",
-                  "O+",
-                  "O-",
-                  "Golden Blood(Rh Null)",
-                ]}
-                {...form.getInputProps("blood_group")}
-              />
-              <Select
-                mb="sm"
-                label="Marital Status"
-                placeholder="Marital Status"
-                required={true}
-                disabled={isSubmitting}
-                data={["Single", "Married", "Widowed", "Divorced", "Separated"]}
-                {...form.getInputProps("marital_status")}
               />
 
               {isMarried && (

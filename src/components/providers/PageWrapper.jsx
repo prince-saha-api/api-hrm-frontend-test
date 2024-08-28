ea5c57927becca81b2939a89fc9053e182e4a7b4
nazmulapi <nazmulhussain.api@gmail.com>
2024-08-28 16:46:49 +0600

    Add preloader

--- src/components/providers/PageWrapper.jsx
+++ src/components/providers/PageWrapper.jsx
@@ -1,19 +1,26 @@
 "use client";
 
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import classEase from "classease";
 import { useSidebar } from "@/components/contexts/SidebarContext";
 import { useUser } from "@/components/contexts/UserContext";
+import Preloader from "@/components/utils/Preloader";
 
 const PageWrapper = ({ children, user }) => {
   const { isSidebarOpen } = useSidebar();
   const { setUser } = useUser();
+  const [isLoaded, setIsLoaded] = useState(false);
 
   useEffect(() => {
     if (user) {
       setUser(user);
     }
+    setIsLoaded(true);
   }, [user]);
+
+  if (!isLoaded) {
+    return <Preloader />;
+  }
 
   return (
     <div className={classEase(isSidebarOpen && "main", "wrapper")}>
